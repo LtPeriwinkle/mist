0be6f32135a9578f20568717771bf66c62e3d6e2
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-12-01 20:31:55 -0700

    rustfmt

--- src/app.rs
+++ src/app.rs
@@ -4,9 +4,9 @@ use sdl2::pixels::Color;
 use sdl2::render::{Texture, WindowCanvas};
 use sdl2::surface::Surface;
 use sdl2::ttf;
+use std::mem;
 use std::thread;
 use std::time::{Duration, Instant};
-use std::mem;
 
 use crate::render;
 use crate::splits::{self, Run};
@@ -14,7 +14,7 @@ use crate::timing;
 
 const SPLITS_ON_SCREEN: usize = 8; //used to limit number of splits displayed
 static mut RECREATE_DEFAULT: Option<u8> = Some(3); // used to determine whether to recreate slice every loop
-// struct that holds information about the running app and its state
+                                                   // struct that holds information about the running app and its state
 #[allow(dead_code)]
 pub struct App {
     context: sdl2::Sdl,
@@ -31,9 +31,9 @@ pub struct App {
 enum TimerState {
     OffsetCountdown { amt: u128 },
     Running { timestamp: u32 },
-    Paused { time: u128, time_str: String},
+    Paused { time: u128, time_str: String },
     NotStarted { time_str: String },
-    Finished {time_str: String}
+    Finished { time_str: String },
 }
 
 impl App {
@@ -61,7 +61,9 @@ impl App {
             timer,
             canvas,
             ttf,
-            state: TimerState::NotStarted {time_str: "".to_string()},
+            state: TimerState::NotStarted {
+                time_str: "".to_string(),
+            },
             run: Run::new(),
         }
     }
@@ -91,13 +93,20 @@ impl App {
         self.run = Run::from_file("test.msf");
         let split_names = &self.run.splits;
         let offset = self.run.offset;
-        match offset {Some(x) => {self.state = TimerState::NotStarted {time_str: format!("-{}", timing::ms_to_readable(x, false))};}, _ => {}}
+        match offset {
+            Some(x) => {
+                self.state = TimerState::NotStarted {
+                    time_str: format!("-{}", timing::ms_to_readable(x, false)),
+                };
+            }
+            _ => {}
+        }
         let split_times_ms: Vec<u128> = self.run.best_times.iter().cloned().collect();
         let split_times_raw: Vec<String> = timing::split_time_sum(split_times_ms);
         let mut text_surface: Surface;
         let mut texture: Texture;
         let mut on_screen: &[Texture] = &[];
-        let on_screen_times: &[Texture] = &[];//: Vec<&Texture> = vec![];
+        let on_screen_times: &[Texture] = &[]; //: Vec<&Texture> = vec![];
         let mut splits: Vec<Texture> = vec![];
         let mut split_times: Vec<Texture> = vec![];
 
@@ -147,9 +156,11 @@ impl App {
         let mut color: Color;
         // sum of split times for display on rows
         let mut recreate_on_screen: Option<u8> = Some(0);
-	if self.run.splits.len() == 0 {
-		unsafe {RECREATE_DEFAULT = None;}
-	}
+        if self.run.splits.len() == 0 {
+            unsafe {
+                RECREATE_DEFAULT = None;
+            }
+        }
         let mut diff: u32 = 0;
         let mut len: usize = splits.len();
         let mut index: usize;
@@ -160,11 +171,13 @@ impl App {
             frame_time = Instant::now();
             self.canvas.set_draw_color(Color::BLACK);
             self.canvas.clear();
-            if let TimerState::OffsetCountdown {amt} = self.state {
-		if amt <= total_time.elapsed().as_millis() {
-			self.state = TimerState::Running {timestamp: self.timer.ticks()};
-			total_time = Instant::now();
-		}
+            if let TimerState::OffsetCountdown { amt } = self.state {
+                if amt <= total_time.elapsed().as_millis() {
+                    self.state = TimerState::Running {
+                        timestamp: self.timer.ticks(),
+                    };
+                    total_time = Instant::now();
+                }
             }
             for event in self.ev_pump.poll_iter() {
                 // print events to terminal if running in debug
@@ -197,33 +210,41 @@ impl App {
                         timestamp: event_time,
                         repeat: false,
                         ..
-                    } => {
-			match self.state {
-				TimerState::Paused {time: t, ..} => {
-    					total_time = Instant::now();
-					before_pause = Some(t);
-					self.state = TimerState::Running {timestamp: event_time};
-				},
-				TimerState::Running {timestamp: t} => {
-					self.state = TimerState::Paused { time: (event_time - t) as u128 + before_pause.unwrap_or(0), time_str: timing::ms_to_readable((event_time - t) as u128 + before_pause.unwrap_or(0), true) };
-				},
-				_ => {}
-			}
-                    }
+                    } => match self.state {
+                        TimerState::Paused { time: t, .. } => {
+                            total_time = Instant::now();
+                            before_pause = Some(t);
+                            self.state = TimerState::Running {
+                                timestamp: event_time,
+                            };
+                        }
+                        TimerState::Running { timestamp: t } => {
+                            self.state = TimerState::Paused {
+                                time: (event_time - t) as u128 + before_pause.unwrap_or(0),
+                                time_str: timing::ms_to_readable(
+                                    (event_time - t) as u128 + before_pause.unwrap_or(0),
+                                    true,
+                                ),
+                            };
+                        }
+                        _ => {}
+                    },
                     Event::KeyDown {
                         keycode: Some(Keycode::R),
                         ..
-                    } => {
-                        match offset {
-				Some(x) => {
-    					before_pause = None;
-					self.state = TimerState::NotStarted {time_str: format!("-{}", timing::ms_to_readable(x, false))};
-				},
-				None => {
-					self.state = TimerState::NotStarted {time_str: "0.000".to_owned()};
-				}
+                    } => match offset {
+                        Some(x) => {
+                            before_pause = None;
+                            self.state = TimerState::NotStarted {
+                                time_str: format!("-{}", timing::ms_to_readable(x, false)),
+                            };
                         }
-                    }
+                        None => {
+                            self.state = TimerState::NotStarted {
+                                time_str: "0.000".to_owned(),
+                            };
+                        }
+                    },
                     Event::Window {
                         win_event: WindowEvent::Resized(..),
                         ..
@@ -243,74 +264,83 @@ impl App {
                             }
                         }
                     }
-                    Event::KeyDown {keycode: Some(Keycode::Space), timestamp: event_time, ..} => {
-			match self.state {
-				TimerState::NotStarted {..} => {
-					total_time = Instant::now();
-					match offset {
-						Some(x) => {self.state = TimerState::OffsetCountdown {amt: x};},
-						None => {self.state = TimerState::Running {timestamp: event_time};}
-					}
-					current_split = 0;
-				},
-				TimerState::Running {timestamp: t, ..} => {
-    					time_str = timing::ms_to_readable((event_time - t) as u128 + before_pause.unwrap_or(0), true);
-    					text_surface = font.render(&time_str).blended(Color::WHITE).unwrap();
-    					texture = creator.create_texture_from_surface(&text_surface).unwrap();
-    					split_times[current_split] = texture;
-					if current_split < splits.len() - 1 {
-						current_split += 1;
-					} else {
-						self.state = TimerState::Finished {time_str};
-					}
-				},
-				_ => {}
-			}
-                    }
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Space),
+                        timestamp: event_time,
+                        ..
+                    } => match self.state {
+                        TimerState::NotStarted { .. } => {
+                            total_time = Instant::now();
+                            match offset {
+                                Some(x) => {
+                                    self.state = TimerState::OffsetCountdown { amt: x };
+                                }
+                                None => {
+                                    self.state = TimerState::Running {
+                                        timestamp: event_time,
+                                    };
+                                }
+                            }
+                            current_split = 0;
+                        }
+                        TimerState::Running { timestamp: t, .. } => {
+                            time_str = timing::ms_to_readable(
+                                (event_time - t) as u128 + before_pause.unwrap_or(0),
+                                true,
+                            );
+                            text_surface = font.render(&time_str).blended(Color::WHITE).unwrap();
+                            texture = creator.create_texture_from_surface(&text_surface).unwrap();
+                            split_times[current_split] = texture;
+                            if current_split < splits.len() - 1 {
+                                current_split += 1;
+                            } else {
+                                self.state = TimerState::Finished { time_str };
+                            }
+                        }
+                        _ => {}
+                    },
                     _ => {}
                 }
             }
             window_width = self.canvas.viewport().width();
             let mut on_screen_times: &[Texture] = &[];
             match recreate_on_screen {
-		Some(0) => {
-			on_screen = &splits[0..bottom_split_index];
-			on_screen_times = &split_times[0..bottom_split_index];
-			unsafe {recreate_on_screen = RECREATE_DEFAULT};
-		},
-		Some(1) => {
-                        if max_splits > diff as usize {
-                            max_splits -= diff as usize;
-                            if current_split + max_splits > len {
-                                bottom_split_index = len;
-                                on_screen = &splits[len - max_splits..bottom_split_index];
-                                on_screen_times = &split_times
-                                    [len - max_splits..bottom_split_index];
-                            } else if current_split < max_splits {
-                                bottom_split_index = max_splits;
-                                on_screen = &splits[0..max_splits];
-                                on_screen_times = &split_times[0..max_splits];
-                            } else if current_split >= max_splits {
-                                bottom_split_index = current_split + max_splits;
-                                on_screen = &splits[current_split..current_split + max_splits];
-                                on_screen_times = &split_times
-                                    [current_split..current_split + max_splits];
-                            }
+                Some(0) => {
+                    on_screen = &splits[0..bottom_split_index];
+                    on_screen_times = &split_times[0..bottom_split_index];
+                    unsafe { recreate_on_screen = RECREATE_DEFAULT };
+                }
+                Some(1) => {
+                    if max_splits > diff as usize {
+                        max_splits -= diff as usize;
+                        if current_split + max_splits > len {
+                            bottom_split_index = len;
+                            on_screen = &splits[len - max_splits..bottom_split_index];
+                            on_screen_times = &split_times[len - max_splits..bottom_split_index];
+                        } else if current_split < max_splits {
+                            bottom_split_index = max_splits;
+                            on_screen = &splits[0..max_splits];
+                            on_screen_times = &split_times[0..max_splits];
+                        } else if current_split >= max_splits {
+                            bottom_split_index = current_split + max_splits;
+                            on_screen = &splits[current_split..current_split + max_splits];
+                            on_screen_times =
+                                &split_times[current_split..current_split + max_splits];
                         }
-			unsafe {recreate_on_screen = RECREATE_DEFAULT}
-		},
-		Some(2) => {
-                        index = bottom_split_index - max_splits;
-                        on_screen = &splits[index..bottom_split_index];
-                        on_screen_times =
-                            &split_times[index..bottom_split_index];
-                        unsafe {recreate_on_screen = RECREATE_DEFAULT}
-		},
-		Some(3) => {
-			index = bottom_split_index - max_splits;
-			on_screen_times = &split_times[index..bottom_split_index];
-		},
-		_ => {}
+                    }
+                    unsafe { recreate_on_screen = RECREATE_DEFAULT }
+                }
+                Some(2) => {
+                    index = bottom_split_index - max_splits;
+                    on_screen = &splits[index..bottom_split_index];
+                    on_screen_times = &split_times[index..bottom_split_index];
+                    unsafe { recreate_on_screen = RECREATE_DEFAULT }
+                }
+                Some(3) => {
+                    index = bottom_split_index - max_splits;
+                    on_screen_times = &split_times[index..bottom_split_index];
+                }
+                _ => {}
             }
             render::render_rows(&on_screen, &on_screen_times, &mut self.canvas, window_width);
             if let TimerState::Running { .. } = self.state {
@@ -333,8 +363,7 @@ impl App {
             if Instant::now().duration_since(frame_time) <= one_sixtieth {
                 thread::sleep(
                     // if the entire loop pass was completed in under 1/60 second, delay to keep the framerate at ~60fps
-                    one_sixtieth
-                        - Instant::now().duration_since(frame_time),
+                    one_sixtieth - Instant::now().duration_since(frame_time),
                 );
             }
         }
@@ -344,21 +373,28 @@ impl App {
         let time: String;
         match &self.state {
             TimerState::Running { .. } => {
-		time = timing::ms_to_readable(total_time.elapsed().as_millis() + before_pause.unwrap_or(0), false);
-            },
+                time = timing::ms_to_readable(
+                    total_time.elapsed().as_millis() + before_pause.unwrap_or(0),
+                    false,
+                );
+            }
             TimerState::Paused {
                 time_str: display, ..
             } => {
                 time = display.to_string();
-            },
-            TimerState::Finished {time_str: string} | TimerState::NotStarted {time_str: string} => {
-		time = string.to_owned();
-            },
-            TimerState::OffsetCountdown {amt: amount} => {
+            }
+            TimerState::Finished { time_str: string }
+            | TimerState::NotStarted { time_str: string } => {
+                time = string.to_owned();
+            }
+            TimerState::OffsetCountdown { amt: amount } => {
                 if amount > &total_time.elapsed().as_millis() {
-			let num = timing::ms_to_readable(amount - total_time.elapsed().as_millis(), false);
-			time = format!("-{}", num);
-                } else { time = "0.000".to_string();}
+                    let num =
+                        timing::ms_to_readable(amount - total_time.elapsed().as_millis(), false);
+                    time = format!("-{}", num);
+                } else {
+                    time = "0.000".to_string();
+                }
             }
         }
         return time;
