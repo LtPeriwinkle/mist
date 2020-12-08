443f6e0e3c80e08b33be1d0450f0ba6ae30dd667
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-12-07 18:29:38 -0700

    tweak making time value

--- src/components.rs
+++ src/components.rs
@@ -1,7 +1,7 @@
 use sdl2::pixels::Color;
 
 pub const SPLITS_ON_SCREEN: usize = 8; // max splits allowed on screen
-pub static MAKING_UP_TIME: Color = Color::RGB(255, 60, 60); // color used when behind but gaining
+pub static MAKING_UP_TIME: Color = Color::RGB(255, 90, 90); // color used when behind but gaining
 pub static LOSING_TIME: Color = Color::RGB(135, 255, 135); // color used when ahead but losing
 
 // state of timer, might implement real state switching eventually
