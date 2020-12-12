ca244f5635fb4d91e9b75add5adcfb08cd8259b1
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-12-11 20:37:27 -0700

    add gold color

--- src/components.rs
+++ src/components.rs
@@ -6,7 +6,7 @@ use tinyfiledialogs as tfd;
 pub const SPLITS_ON_SCREEN: usize = 8; // max splits allowed on screen
 pub static MAKING_UP_TIME: Color = Color::RGB(255, 90, 90); // color used when behind but gaining
 pub static LOSING_TIME: Color = Color::RGB(135, 255, 135); // color used when ahead but losing
-
+pub static GOLD: Color = Color::RGB(255, 255, 0); // default for when beating best split time
 // state of timer, might implement real state switching eventually
 #[derive(Debug)]
 pub enum TimerState {
