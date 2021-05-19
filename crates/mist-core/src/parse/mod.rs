50f5eb617f93b09d824a77bc0f7d7701eb6468a4
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-05-19 15:14:12 -0700

    [core] remove parser generics

--- crates/mist-core/src/parse/mod.rs
+++ crates/mist-core/src/parse/mod.rs
@@ -1,5 +1,8 @@
 //! Parse [Run](crate::run::Run)s from their file representations.
 mod msf;
-pub use msf::MsfParser;
+pub use msf::{MsfParser, MsfWriter};
+
+#[cfg(feature = "lss")]
 mod lss;
+#[cfg(feature = "lss")]
 pub use lss::LssParser;
--- crates/mist-core/src/parse/msf.rs
+++ crates/mist-core/src/parse/msf.rs
@@ -1,22 +1,24 @@
 use crate::run::Run;
 use ron::de::from_bytes;
-use std::io::BufRead;
+use ron::ser::{to_writer_pretty, PrettyConfig};
+use std::io::{BufRead, BufReader, Read};
 use std::io::{Error, ErrorKind};
+use std::fs::File;
 
 /// Parses the version and [Run] from a mist split file (msf)
-pub struct MsfParser<R: BufRead> {
-    reader: R,
+pub struct MsfParser {
+    reader: BufReader<File>,
     version: u32,
 }
 
-impl<R: BufRead> MsfParser<R> {
+impl MsfParser {
     const LEGACY: u32 = 0xBA5ED;
     /// Create a new MsfParser with the given reader. Reader must implement [BufRead].
     ///
     /// # Errors
     ///
     /// * If a line cannot be read from the reader.
-    pub fn new(mut reader: R) -> Result<Self, Error> {
+    pub fn new(mut reader: BufReader<File>) -> Result<Self, Error> {
         let mut ver_info = String::new();
         while ver_info.is_empty() {
             reader.read_line(&mut ver_info)?;
@@ -26,6 +28,9 @@ impl<R: BufRead> MsfParser<R> {
             None => Self::LEGACY,
         };
         Ok(MsfParser { reader, version })
+    }
+    pub fn set_reader(&mut self, reader: BufReader<File>) {
+        self.reader = reader
     }
     /// Determine whether the Run will need converting to the current version, i.e. it is legacy or just outdated.
     pub fn needs_converting(&self) -> bool {
@@ -49,5 +54,19 @@ impl<R: BufRead> MsfParser<R> {
         // TODO: better error handling
         let _ = self.reader.read_to_end(&mut data)?;
         from_bytes(&mut data).map_err(|e| {Error::new(ErrorKind::InvalidData, format!("{}", e))})
+    }
+}
+
+pub struct MsfWriter {
+    writer: File,
+    config: PrettyConfig
+}
+
+impl MsfWriter {
+    pub fn new(writer: File) -> Self {
+        MsfWriter {writer, config: PrettyConfig::new().with_enumerate_arrays(true)}
+    }
+    pub fn write(&mut self, run: Run) -> Result<(), String> {
+        to_writer_pretty(&mut self.writer, &run, self.config.clone()).map_err(|e| {e.to_string()})
     }
 }
