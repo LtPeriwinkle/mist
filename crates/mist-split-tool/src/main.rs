75f16d4e36429314e549059847b761b2b30f5611
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-05-23 09:50:45 -0700

    [tool] cobble together newish split tool

--- crates/mist-split-tool/src/main.rs
+++ crates/mist-split-tool/src/main.rs
@@ -1,7 +1,6 @@
 use fltk::{app, button, dialog, draw, input, table, window::*};
-use mist_run_utils::run::Run;
+use mist_core::{parse::LssParser, parse::MsfParser, Run};
 use std::convert::TryInto;
-use std::env::current_dir;
 use std::sync::Mutex;
 use tinyfiledialogs as tfd;
 
@@ -10,7 +9,7 @@ use lazy_static::lazy_static;
 static HEADERS: [&'static str; 3] = ["Split Name", "Personal Best", "Gold"];
 
 lazy_static! {
-    static ref RUN: Mutex<Run> = Mutex::new(Run::default());
+    static ref RUN: Mutex<Run> = Mutex::new(Run::empty());
     static ref VECS: Mutex<(Vec<u128>, Vec<u128>, Vec<String>)> =
         Mutex::new((vec![], vec![], vec![]));
 }
@@ -18,26 +17,15 @@ lazy_static! {
 static mut ILLEGAL: bool = false;
 
 fn open_split_file() -> Option<String> {
-    let cwd = current_dir().unwrap();
-    let mut dir = cwd.to_string_lossy();
-    dir.to_mut().push('/');
-    return tfd::open_file_dialog(
+    tfd::open_file_dialog(
         "Open a split file (msf or lss)",
-        &dir,
+        "",
         Some((&["*.msf", "*.lss"], "")),
-    );
+    )
 }
-
 fn get_save_as() -> Option<String> {
-    let cwd = current_dir().unwrap();
-    let mut dir = cwd.to_string_lossy();
-    dir.to_mut().push('/');
-    match tfd::save_file_dialog_with_filter(
-        "Save to MSF file",
-        &dir,
-        &["*.msf"],
-        "mist split files",
-    ) {
+    match tfd::save_file_dialog_with_filter("Save to MSF file", "", &["*.msf"], "mist split files")
+    {
         Some(mut p) => {
             if p.ends_with(".msf") {
                 Some(p)
@@ -46,7 +34,7 @@ fn get_save_as() -> Option<String> {
                 Some(p)
             }
         }
-        None => None
+        None => None,
     }
 }
 
@@ -80,25 +68,25 @@ fn str_to_ms(tm: String) -> u128 {
         ILLEGAL = false;
     }
     let mut ms: u128 = 0;
-    let alert = {
+    fn alert(_: std::num::ParseIntError) -> u128  {
         dialog::alert_default("invalid time entered");
         unsafe {
             ILLEGAL = true;
         }
         0
-    };
+    }
     let split: Vec<&str> = tm.split(':').collect();
     if split.len() == 2 {
-        ms += split[0].parse::<u128>().unwrap_or(alert) * 60000;
+        ms += split[0].parse::<u128>().unwrap_or_else(alert) * 60000;
         let split2: Vec<&str> = split[1].split('.').collect();
-        ms += split2[0].parse::<u128>().unwrap_or(alert) * 1000;
-        ms += split2[1].parse::<u128>().unwrap_or(alert);
+        ms += split2[0].parse::<u128>().unwrap_or_else(alert) * 1000;
+        ms += split2[1].parse::<u128>().unwrap_or_else(alert);
     } else if split.len() == 3 {
-        ms += split[0].parse::<u128>().unwrap_or(alert) * 3600000;
-        ms += split[1].parse::<u128>().unwrap_or(alert) * 60000;
+        ms += split[0].parse::<u128>().unwrap_or_else(alert) * 3600000;
+        ms += split[1].parse::<u128>().unwrap_or_else(alert) * 60000;
         let split2: Vec<&str> = split[2].split('.').collect();
-        ms += split2[0].parse::<u128>().unwrap_or(alert) * 1000;
-        ms += split2[1].parse::<u128>().unwrap_or(alert);
+        ms += split2[0].parse::<u128>().unwrap_or_else(alert) * 1000;
+        ms += split2[1].parse::<u128>().unwrap_or_else(alert);
     }
     return ms;
 }
@@ -108,15 +96,30 @@ fn main() {
     let mut save_path: String = "".to_string();
     match path {
         Some(ref p) => {
+            let run: Run;
             if p.ends_with(".msf") {
-                *RUN.lock().unwrap() = Run::from_msf_file(&p).unwrap();
+                let parser = MsfParser::new();
+                let f = std::fs::File::open(p).unwrap();
+                run = parser.parse(std::io::BufReader::new(f)).unwrap();
+                *VECS.lock().unwrap() = (
+                    run.pb_times().to_owned(),
+                    run.gold_times().to_owned(),
+                    run.splits().to_owned(),
+                );
+                *RUN.lock().unwrap() = run;
             } else {
-                *RUN.lock().unwrap() = Run::from_lss_file(&p).unwrap();
+                let f = std::fs::File::open(p).unwrap();
+                let mut parser = LssParser::new(std::io::BufReader::new(f));
+                run = parser.parse();
+                *VECS.lock().unwrap() = (
+                    run.pb_times().to_owned(),
+                    run.gold_times().to_owned(),
+                    run.splits().to_owned(),
+                );
+                *RUN.lock().unwrap() = run;
             }
         }
-        None => {
-            std::process::exit(0);
-        }
+        None => return,
     }
     let app = app::App::default();
     let mut win = Window::default()
@@ -124,7 +127,7 @@ fn main() {
         .center_screen()
         .with_label("mist split editor");
     let mut table = table::Table::new(5, 50, 503, 550, "");
-    let og_len: u32 = RUN.lock().unwrap().split_names().len().try_into().unwrap();
+    let og_len: u32 = RUN.lock().unwrap().splits().len().try_into().unwrap();
     table.set_rows(og_len);
     table.set_row_header(true);
     table.set_cols(3);
@@ -136,33 +139,72 @@ fn main() {
     let mut save_button = button::Button::new(423, 25, 80, 25, "save file");
     let mut add_button = button::Button::new(342, 25, 80, 25, "add split");
     let mut sub_button = button::Button::new(261, 25, 80, 25, "remove split");
+    let mut open_button = button::Button::new(180, 25, 80, 25, "open file");
     win.make_resizable(false);
     win.end();
     win.show();
+    let mut tbl = table.clone();
+    open_button.set_callback(move || {
+        let path = open_split_file();
+        match path {
+            Some(ref p) => {
+                if p.ends_with(".msf") {
+                    let parser = MsfParser::new();
+                    let f = std::fs::File::open(p).unwrap();
+                    let run = parser.parse(std::io::BufReader::new(f)).unwrap();
+                    *VECS.lock().unwrap() = (
+                        run.pb_times().to_owned(),
+                        run.gold_times().to_owned(),
+                        run.splits().to_owned(),
+                    );
+                    *RUN.lock().unwrap() = run;
+                } else {
+                    let f = std::fs::File::open(p).unwrap();
+                    let mut parser = LssParser::new(std::io::BufReader::new(f));
+                    let run = parser.parse();
+                    *VECS.lock().unwrap() = (
+                        run.pb_times().to_owned(),
+                        run.gold_times().to_owned(),
+                        run.splits().to_owned(),
+                    );
+                    *RUN.lock().unwrap() = run;
+                }
+            }
+            None => return,
+        }
+        fltk::TableExt::clear(&mut tbl);
+        tbl.set_rows(og_len);
+        tbl.set_row_header(true);
+        tbl.set_cols(3);
+        tbl.set_col_header(true);
+        tbl.set_col_width(0, 180);
+        tbl.set_col_width(1, 140);
+        tbl.set_col_width(2, 140);
+    });
     save_button.set_callback(move || {
-        let mut vecs = VECS.lock().unwrap();
+        let vecs = VECS.lock().unwrap();
         let mut run = RUN.lock().unwrap();
-        let mut times = run.get_times().clone();
-        let mut golds = run.get_golds().clone();
-        let mut splits = run.split_names().clone();
-        times.append(&mut vecs.0);
-        golds.append(&mut vecs.1);
-        splits.append(&mut vecs.2);
-        run.set_times(&times);
-        run.set_golds(&golds);
-        run.set_names(&splits);
-        run.set_pb(times.iter().sum());
+        run.set_pb_times(&vecs.0);
+        run.set_gold_times(&vecs.1);
+        run.set_splits(&vecs.2);
+        run.set_pb(vecs.1.iter().sum());
+        // fill sum times with empty ones until i figure out how i want to handle it
+        run.set_sum_times(&vecs.2.iter().map(|_| (0u128, 0u128)).collect());
         unsafe {
             if !ILLEGAL {
+                let parser = MsfParser::new();
+                let f: std::fs::File;
                 if save_path != "".to_string() {
-                    run.save_msf(&save_path);
+                    f = std::fs::File::create(save_path.clone()).unwrap();
+                    parser.write(&run, f).unwrap();
                 } else {
                     let name = get_save_as();
                     match name {
-                        Some(p) => {
+                        Some(ref p) => {
                             if p != "()" {
-                                run.save_msf(&p);
-                                save_path = p;
+                                f = std::fs::File::create(p).unwrap();
+                                parser.write(&run, f).unwrap();
+                                save_path = p.to_owned();
                             }
                         }
                         None => {}
@@ -175,7 +217,7 @@ fn main() {
     });
     table.draw_cell2(move |t, ctx, row, col, x, y, w, h| {
         match ctx {
-            table::TableContext::StartPage => draw::set_font(Font::Courier, 14),
+            table::TableContext::StartPage => draw::set_font(Font::Helvetica, 14),
             table::TableContext::ColHeader => {
                 draw::push_clip(x, y, w, h);
                 draw::draw_box(FrameType::ThinUpBox, x, y, w, h, Color::FrameDefault);
@@ -192,29 +234,41 @@ fn main() {
             }
             table::TableContext::Cell => {
                 let mut inp = input::Input::new(x, y, w, h, "");
-                if row < (og_len as i32) {
+                if row < ((VECS.lock().unwrap().2.len()) as i32) {
                     if col == 0 {
-                        inp.set_value(&RUN.lock().unwrap().split_names()[row as usize]);
+                        inp.set_value(&VECS.lock().unwrap().2[row as usize]);
                         inp.set_callback2(move |input| {
-                            RUN.lock().unwrap().set_name(input.value(), row as usize);
+                            if (row as usize) >= VECS.lock().unwrap().2.len() {
+                                VECS.lock().unwrap().2.push(input.value())
+                            } else {
+                                VECS.lock().unwrap().2[row as usize] = input.value();
+                            }
                         })
                     } else if col == 1 {
-                        inp.set_value(&ms_to_readable(
-                            RUN.lock().unwrap().get_times()[row as usize],
-                        ));
+                        if (row as usize) < VECS.lock().unwrap().0.len() {
+                            inp.set_value(&ms_to_readable(
+                                VECS.lock().unwrap().0[row as usize],
+                            ));
+                        }
                         inp.set_callback2(move |input| {
-                            RUN.lock()
-                                .unwrap()
-                                .set_time(str_to_ms(input.value()), row as usize);
+                            if (row as usize) >= VECS.lock().unwrap().0.len() {
+                                VECS.lock().unwrap().0.push(str_to_ms(input.value()))
+                            } else {
+                                VECS.lock().unwrap().0[row as usize] = str_to_ms(input.value());
+                            }
                         })
                     } else if col == 2 {
-                        inp.set_value(&ms_to_readable(
-                            RUN.lock().unwrap().get_golds()[row as usize],
-                        ));
+                        if (row as usize) < VECS.lock().unwrap().1.len() {
+                            inp.set_value(&ms_to_readable(
+                                VECS.lock().unwrap().1[row as usize],
+                            ));
+                        }
                         inp.set_callback2(move |input| {
-                            RUN.lock()
-                                .unwrap()
-                                .set_gold_time(row as usize, str_to_ms(input.value()));
+                            if (row as usize) >= VECS.lock().unwrap().1.len() {
+                                VECS.lock().unwrap().1.push(str_to_ms(input.value()))
+                            } else {
+                                VECS.lock().unwrap().1[row as usize] = str_to_ms(input.value());
+                            }
                         })
                     }
                 } else {
@@ -225,7 +279,6 @@ fn main() {
                             } else {
                                 VECS.lock().unwrap().2.insert(row as usize, input.value());
                             }
-                            //RUN.lock().unwrap().set_name(input.value(), row as usize);
                         })
                     } else if col == 1 {
                         inp.set_callback2(move |input| {
@@ -262,7 +315,17 @@ fn main() {
         table.redraw();
     });
     sub_button.set_callback(move || {
-        table1.set_rows(table1.rows() - 1);
+        let new_rows = table1.rows() - 1;
+        if (new_rows as usize) < VECS.lock().unwrap().0.len() {
+            VECS.lock().unwrap().0.pop();
+        }
+        if (new_rows as usize) < VECS.lock().unwrap().1.len() {
+            VECS.lock().unwrap().1.pop();
+        }
+        if (new_rows as usize) < VECS.lock().unwrap().2.len() {
+            VECS.lock().unwrap().2.pop();
+        }
+        table1.set_rows(new_rows);
         table1.redraw();
     });
     app.run().unwrap();
