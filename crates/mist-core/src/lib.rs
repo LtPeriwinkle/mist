be4eaaa1cf6d7e37c59588967364b7d80c88ca2f
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-05-29 13:05:01 -0700

    [core] put keybinds behind config feature

--- crates/mist-core/src/lib.rs
+++ crates/mist-core/src/lib.rs
@@ -2,7 +2,9 @@
 /// Dialog boxes to prompt the user for things.
 pub mod dialogs;
 pub mod parse;
+#[cfg(feature = "config")]
 mod keybinds;
+#[cfg(feature = "config")]
 pub use keybinds::KeybindsRaw;
 mod run;
 #[cfg(feature = "timing")]
