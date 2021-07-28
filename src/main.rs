2f03e3bcb28f12cf2f7345abac76df0b4cd2c773
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-07-28 10:53:04 -0700

    remove quotes from error box text

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,10 @@ use mist_core::dialogs::error;
 
 fn main() {
     std::panic::set_hook(Box::new(|info| {
-        println!("{}", info);
-        error(&format!("{}", info));
+        let out = format!("{}", info);
+        let out = out.replace('\'', "");
+        println!("{}", out);
+        error(&out);
     }));
     let context = sdl2::init().unwrap_or_else(|err| {
         error(&err);
