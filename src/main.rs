4ad4029a3026a5a29c135f868296da381e054ab3
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-07-28 10:32:51 -0700

    print panic info on panic

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,8 @@ use mist_core::dialogs::error;
 
 fn main() {
     std::panic::set_hook(Box::new(|info| {
-        let mut out = String::from("mist panicked: ");
-        if let Some(payload) = info.payload().downcast_ref::<String>() {
-            out.push_str(&format!("({})", payload));
-        }
-        if let Some(loc) = info.location() {
-            out.push_str(&format!(" in {}:{}", loc.file(), loc.line()));
-        }
-        error(&out);
+        println!("{}", info);
+        error(&format!("{}", info));
     }));
     let context = sdl2::init().unwrap_or_else(|err| {
         error(&err);
