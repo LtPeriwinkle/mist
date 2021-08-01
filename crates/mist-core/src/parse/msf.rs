88218b87e0567606bc3e7d8f1a3e1bf4302dbf40
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-08-01 11:22:26 -0700

    [core] add run sanity checks (fixes #6)
    MsfParser will now check for the right number of pb times
    and gold times before either returning a run or writing one.
    If there are too few, they will be padded with zeroes.
    Having too many is ignored, because currently mist will
    only work with as many times as there are split names.

--- crates/mist-core/src/parse/msf.rs
+++ crates/mist-core/src/parse/msf.rs
@@ -68,17 +68,42 @@ impl MsfParser {
             }
             s
         };
-        if version == 0 {
-            let legacy: LegacyRun = from_str(&mut data).map_err(|e| e.to_string())?;
-            return Ok(legacy.into());
-        }
-        from_str(&mut data).map_err(|e| e.to_string())
+        let run = self.run_sanity(if version == 0 {
+            from_str::<LegacyRun>(&data)
+                .map_err(|e| e.to_string())?
+                .into()
+        } else {
+            from_str(&data).map_err(|e| e.to_string())?
+        });
+        Ok(run)
     }
     /// Write the given run to the given writer.
     pub fn write<W: Write>(&self, run: &Run, mut writer: W) -> Result<(), String> {
+        let run = self.run_sanity(run.clone());
         writer.write(b"version 1\n").map_err(|e| e.to_string())?;
-        to_writer_pretty(&mut writer, run, PrettyConfig::new()).map_err(|e| e.to_string())?;
+        to_writer_pretty(&mut writer, &run, PrettyConfig::new()).map_err(|e| e.to_string())?;
         Ok(())
+    }
+
+    fn run_sanity(&self, run: Run) -> Run {
+        let mut run = run;
+        let len = run.splits().len();
+        let mut golds = run.gold_times().to_owned();
+        let mut times = run.pb_times().to_owned();
+        let mut sums = run.sum_times().to_owned();
+        if golds.len() < len {
+            golds.resize_with(len, Default::default);
+            run.set_gold_times(&golds);
+        }
+        if times.len() < len {
+            times.resize_with(len, Default::default);
+            run.set_pb_times(&times);
+        }
+        if sums.len() < len {
+            sums.resize_with(len, Default::default);
+            run.set_sum_times(&sums);
+        }
+        run
     }
 }
 
@@ -121,5 +146,29 @@ mod tests {
         let parser = MsfParser::new();
         let run = parser.parse(reader);
         assert!(run.is_ok());
+    }
+
+    const INSANE_RUN: &[u8] = b"version 1\n
+        (
+            game_title: \"test\",
+            category: \"test\",
+            offset: Some(200),
+            pb: 1234,
+            splits: [\"test\", \"test2\"],
+            pb_times: [1234],
+            gold_times: [1234],
+            sum_times: [(2, 1234)],
+        )";
+
+    #[test]
+    fn test_sanity_check() {
+        let reader = std::io::BufReader::new(INSANE_RUN);
+        let parser = MsfParser::new();
+        let run = parser.parse(reader);
+        assert!(run.is_ok());
+        let run = run.unwrap();
+        assert_eq!(run.gold_times().to_owned(), vec![1234, 0]);
+        assert_eq!(run.pb_times().to_owned(), vec![1234, 0]);
+        assert_eq!(run.sum_times().to_owned(), vec![(2, 1234), (0, 0)]);
     }
 }
