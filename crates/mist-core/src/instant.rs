313639f9d1b4341e361878ac443ff03361b64d52
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-08-21 20:07:09 -0700

    [core] linux: fix overflow on MistInstant subtraction

--- crates/mist-core/src/instant.rs
+++ crates/mist-core/src/instant.rs
@@ -4,78 +4,134 @@ pub use inner::MistInstant;
 #[cfg(unix)]
 mod inner {
     pub use innerinner::MistInstant;
+
     #[cfg(any(target_os = "macos", target_os = "ios"))]
     mod innerinner {
         #[derive(Copy, Clone)]
         pub struct MistInstant {
-            t: u64
+            t: u64,
         }
+
         impl MistInstant {
             pub fn now() -> Self {
                 extern "C" {
                     fn mach_continuous_time() -> u64;
                 }
-                MistInstant {t: unsafe {mach_continuous_time()}}
+                MistInstant {
+                    t: unsafe { mach_continuous_time() },
+                }
             }
         }
+
         impl std::ops::Sub<MistInstant> for MistInstant {
             type Output = std::time::Duration;
             fn sub(self, other: MistInstant) -> Self::Output {
-                self.t.checked_sub(other.t).expect("overflow when subtracting instants")
+                self.t
+                    .checked_sub(other.t)
+                    .expect("overflow when subtracting instants")
             }
         }
     }
-    #[cfg(not(any(target_os = "macos", target_os = "ios", windows)))]
+
+    #[cfg(not(any(target_os = "macos", target_os = "ios")))]
     mod innerinner {
         #[derive(Copy, Clone)]
         struct Timespec {
-            t: libc::timespec
+            t: libc::timespec,
         }
+
+        impl PartialEq for Timespec {
+            fn eq(&self, other: &Timespec) -> bool {
+                self.t.tv_sec == other.t.tv_sec && self.t.tv_nsec == other.t.tv_nsec
+            }
+        }
+
+        impl Eq for Timespec {}
+
+        impl PartialOrd for Timespec {
+            fn partial_cmp(&self, other: &Timespec) -> Option<std::cmp::Ordering> {
+                Some(self.cmp(other))
+            }
+        }
+
+        impl Ord for Timespec {
+            fn cmp(&self, other: &Timespec) -> std::cmp::Ordering {
+                let me = (self.t.tv_sec, self.t.tv_nsec);
+                let other = (other.t.tv_sec, other.t.tv_nsec);
+                me.cmp(&other)
+            }
+        }
+
         impl Timespec {
             fn sub_timespec(&self, other: &Timespec) -> Option<std::time::Duration> {
-                if self.t.tv_sec >= other.t.tv_sec && self.t.tv_nsec >= other.t.tv_nsec {
-                    let (sec, nsec) = ((self.t.tv_sec - other.t.tv_sec) as u64, (self.t.tv_nsec - other.t.tv_nsec) as u32);
-                    Some(std::time::Duration::new(sec, nsec))
+                if self >= other {
+                    let (secs, nsec) = if self.t.tv_nsec >= other.t.tv_nsec {
+                        (
+                            (self.t.tv_sec - other.t.tv_sec) as u64,
+                            (self.t.tv_nsec - other.t.tv_nsec) as u32,
+                        )
+                    } else {
+                        (
+                            (self.t.tv_sec - other.t.tv_sec - 1) as u64,
+                            self.t.tv_nsec as u32 + 1_000_000_000u32 - other.t.tv_nsec as u32,
+                        )
+                    };
+                    Some(std::time::Duration::new(secs, nsec))
                 } else {
                     None
                 }
             }
         }
+
+
         #[derive(Copy, Clone)]
         pub struct MistInstant {
-            t: Timespec
+            t: Timespec,
         }
+        
         impl MistInstant {
             pub fn now() -> Self {
-                Self {t: now(libc::CLOCK_BOOTTIME)}
+                Self {
+                    t: now(libc::CLOCK_BOOTTIME),
+                }
             }
         }
+
         #[cfg(not(any(target_os = "dragonfly", target_os = "espidf")))]
         pub type clock_t = libc::c_int;
         #[cfg(any(target_os = "dragonfly", target_os = "espidf"))]
         pub type clock_t = libc::c_ulong;
+
         fn now(clock: clock_t) -> Timespec {
-            let mut t = Timespec {t: libc::timespec {tv_sec: 0, tv_nsec: 0}};
-            let r = unsafe {libc::clock_gettime(clock, &mut t.t)};
+            let mut t = Timespec {
+                t: libc::timespec {
+                    tv_sec: 0,
+                    tv_nsec: 0,
+                },
+            };
+            let r = unsafe { libc::clock_gettime(clock, &mut t.t) };
             if r == -1 {
                 panic!("couldn't clock_gettime");
             }
             t
         }
+
         impl std::ops::Sub<MistInstant> for MistInstant {
             type Output = std::time::Duration;
             fn sub(self, other: MistInstant) -> Self::Output {
-                self.t.sub_timespec(&other.t).expect("overflow when subtracting instants")
+                self.t
+                    .sub_timespec(&other.t)
+                    .expect("overflow when subtracting instants")
             }
         }
     }
+
     impl MistInstant {
         pub fn elapsed(&self) -> std::time::Duration {
             Self::now() - *self
         }
     }
 }
-
 
 #[cfg(windows)]
 mod inner {
@@ -89,4 +145,3 @@ mod inner {
         }
     }
 }
-
