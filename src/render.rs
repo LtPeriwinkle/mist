af707d912184608989ea674db9a6d6d76e2d81db
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-12-29 17:26:40 -0700

    start on RenderState

--- src/render.rs
+++ src/render.rs
@@ -1,18 +1,214 @@
 // Functions for putting stuff into the correct places on the sdl buffer
 use crate::panels::RenderPanel;
 use crate::splits::Split;
+use mist_core::config::{Config, Panel};
+use mist_core::{timing, Run};
+use sdl2::get_error;
+#[cfg(feature = "bg")]
+use sdl2::gfx::rotozoom::RotozoomSurface;
+#[cfg(feature = "bg")]
+use sdl2::image::LoadSurface;
 use sdl2::pixels::Color;
+#[cfg(feature = "bg")]
+use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::{Point, Rect};
-use sdl2::render::{Canvas, Texture, TextureQuery};
-use sdl2::video::Window;
+use sdl2::render::{Texture, TextureCreator, TextureQuery, WindowCanvas};
+use sdl2::rwops::RWops;
+use sdl2::surface::Surface;
+use sdl2::ttf::{self, Font, Sdl2TtfContext};
+use sdl2::video::WindowContext;
+use std::cell::RefCell;
 use std::convert::TryInto;
+use std::rc::Rc;
+
+pub struct RenderState<'a> {
+    run: Rc<RefCell<Run>>,
+    canvas: WindowCanvas,
+    creator: TextureCreator<WindowContext>,
+    ttf: Sdl2TtfContext,
+    colors: [(u8, u8, u8); 6],
+    splits: Vec<Split<'a>>,
+    panels: Vec<RenderPanel<'a>>,
+    map: FontMap<'a>,
+    timer_font: Font<'a, 'a>,
+    splits_font: Font<'a, 'a>,
+    #[cfg(feature = "bg")]
+    background: Background<'a>,
+}
+
+#[cfg(feature = "bg")]
+enum Background<'a> {
+    NoBackground,
+    HasBackground { tex: Texture<'a>, rect: Rect },
+}
+
+struct FontMap<'a> {
+    tex: Texture<'a>,
+    coords: Vec<u32>,
+}
+
+impl<'a> RenderState<'a> {
+    pub fn new(
+        run: Rc<RefCell<Run>>,
+        canvas: WindowCanvas,
+        config: &Config,
+    ) -> Result<Self, String> {
+        let ttf = ttf::init().map_err(|_| get_error())?;
+        let creator = canvas.texture_creator();
+        let rw = RWops::from_file(config.tfont().get_path()?, "r")?;
+        let timer_font = ttf.load_font_from_rwops(rw, config.fsize().0)?;
+        let rw = RWops::from_file(config.sfont().get_path()?, "r")?;
+        let splits_font = ttf.load_font_from_rwops(rw, config.fsize().1)?;
+        let panels = {
+            let mut ret = vec![];
+            for panel in config.panels() {
+                let (text, paneltype) = match panel {
+                    Panel::Pace { golds } => {
+                        if *golds {
+                            ("Pace (best)", Panel::Pace { golds: true })
+                        } else {
+                            ("Pace (pb)", Panel::Pace { golds: false })
+                        }
+                    }
+                    Panel::SumOfBest => ("Sum of Best", Panel::SumOfBest),
+                    Panel::CurrentSplitDiff { golds } => {
+                        if *golds {
+                            ("Split (best)", Panel::CurrentSplitDiff { golds: true })
+                        } else {
+                            ("Split (pb)", Panel::CurrentSplitDiff { golds: false })
+                        }
+                    }
+                };
+                let time = if let Panel::SumOfBest = panel {
+                    let sob = run.borrow().gold_times().iter().sum::<u128>();
+                    timing::split_time_text(sob)
+                } else {
+                    "-  ".into()
+                };
+                let time_tex = render_white_text(&time, &splits_font, &creator)?;
+                let text_tex = render_white_text(&text, &splits_font, &creator)?;
+                let newpanel = RenderPanel::new(text_tex, time_tex, paneltype);
+                ret.push(newpanel);
+            }
+            ret
+        };
+        Ok(Self {
+            run,
+            canvas,
+            creator,
+            ttf,
+            colors: config.color_list(),
+            panels,
+            map: FontMap::generate(&timer_font, &creator, Color::WHITE)?,
+            timer_font,
+            splits_font,
+            background: Background::load(config, canvas.viewport(), creator)?,
+        })
+    }
+}
+
+impl<'a> FontMap<'a> {
+    fn generate(
+        font: &Font<'a, 'a>,
+        creator: &'a TextureCreator<WindowContext>,
+        color: Color,
+    ) -> Result<Self, String> {
+        let mut max = 0;
+        let mut sum = 0;
+        let mut coords = vec![0];
+        for chr in "-0123456789:.".chars() {
+            let temp = font.size_of(&chr.to_string()).map_err(|_| get_error())?.0;
+            sum += temp;
+            if temp > max {
+                max = temp
+            };
+            coords.push(sum);
+        }
+        let sur = font
+            .render("- 0 1 2 3 4 5 6 7 8 9 : .")
+            .blended(color)
+            .map_err(|_| get_error())?;
+        Ok(Self {
+            tex: creator
+                .create_texture_from_surface(&sur)
+                .map_err(|_| get_error())?,
+            coords,
+        })
+    }
+}
+
+impl Background<'_> {
+    fn load(
+        config: &Config,
+        viewport: Rect,
+        creator: TextureCreator<WindowContext>,
+    ) -> Result<Self, String> {
+        let bg: Option<Surface> = match config.img() {
+            Some(ref p) => Some(Surface::from_file(&p)?),
+            None => None,
+        };
+        if let Some(x) = bg {
+            let bg_tex: Texture;
+            let width = viewport.width();
+            let height = viewport.height();
+            if !config.img_scaled() {
+                let mut sur = Surface::new(width, height, PixelFormatEnum::RGB24)?;
+                let cutoffx = {
+                    if x.width() > width {
+                        ((x.width() - width) / 2) as i32
+                    } else {
+                        0
+                    }
+                };
+                let cutoffy = {
+                    if x.height() > height {
+                        ((x.height() - height) / 2) as i32
+                    } else {
+                        0
+                    }
+                };
+                x.blit(Rect::new(cutoffx, cutoffy, width, height), &mut sur, None)?;
+                bg_tex = creator
+                    .create_texture_from_surface(&sur)
+                    .map_err(|_| get_error())?;
+            } else {
+                let sur: Surface;
+                if x.width() > x.height() && width < x.width() {
+                    if width < x.width() {
+                        sur = x.rotozoom(0.0, width as f64 / x.width() as f64, true)?;
+                    } else {
+                        sur = x.rotozoom(0.0, x.width() as f64 / width as f64, true)?;
+                    }
+                } else if height < x.height() {
+                    sur = x.rotozoom(0.0, height as f64 / x.height() as f64, true)?;
+                } else {
+                    sur = x.rotozoom(0.0, x.height() as f64 / height as f64, true)?;
+                }
+                bg_tex = creator
+                    .create_texture_from_surface(&sur)
+                    .map_err(|_| get_error())?;
+            }
+            let sdl2::render::TextureQuery {
+                width: bgw,
+                height: bgh,
+                ..
+            } = bg_tex.query();
+            Ok(Background::HasBackground {
+                tex: bg_tex,
+                rect: Rect::new(0, 0, bgw, bgh),
+            })
+        } else {
+            Ok(Background::NoBackground)
+        }
+    }
+}
 
 // Puts split name textures and their associated times into the SDL backbuffer
 // handles placing all the textures around the other ones and highlighting the active split based on the
 // index passed to it
 pub fn render_rows(
     on_screen: &[Split],
-    canvas: &mut Canvas<Window>,
+    canvas: &mut WindowCanvas,
     (split_height, inline): (u32, bool),
     current: usize,
 ) -> Result<(), String> {
@@ -46,18 +242,18 @@ pub fn render_rows(
                 tinfo
             }
             None => {
-                let tinfo = item.comp_texture().query();
+                let tinfo = item.comp().query();
                 row = Rect::new(
                     (window_width - tinfo.width) as i32,
                     num_y,
                     tinfo.width,
                     tinfo.height,
                 );
-                canvas.copy(&item.comp_texture(), None, Some(row))?;
+                canvas.copy(&item.comp(), None, Some(row))?;
                 tinfo
             }
         };
-        match item.diff_texture() {
+        match item.diff() {
             None => {}
             Some(x) => {
                 let TextureQuery {
@@ -90,7 +286,7 @@ pub fn render_time(
     atlas: &Texture,
     coords: &[(u32, u32, u32, u32)],
     (font_y, splits_height, num_panels): (u32, u32, usize),
-    canvas: &mut Canvas<Window>,
+    canvas: &mut WindowCanvas,
 ) -> Result<(), String> {
     let vp = canvas.viewport();
     let h = vp.height();
@@ -158,7 +354,7 @@ pub fn get_coords(time_str: String, coords: &[u32]) -> Vec<(u32, u32, u32, u32)>
     ret
 }
 
-pub fn render_panels(panels: &[RenderPanel], canvas: &mut Canvas<Window>) -> Result<(), String> {
+pub fn render_panels(panels: &[RenderPanel], canvas: &mut WindowCanvas) -> Result<(), String> {
     let mut num = 1;
     for panel in panels {
         let TextureQuery { width, height, .. } = panel.text().query();
--- src/splits.rs
+++ src/splits.rs
@@ -1,73 +1,45 @@
-// split struct and methods
 use sdl2::render::Texture;
 
-// this holds a lot of information about both what to render and how to compare to it
 pub struct Split<'a> {
-    pb_time: u128,
-    gold_time: u128,
-    diff: i128,
-    diff_texture: Option<Texture<'a>>,
-    name_texture: Texture<'a>,
-    comp_texture: Texture<'a>,
-    current_texture: Option<Texture<'a>>,
+    diff: Option<Texture<'a>>,
+    name: Texture<'a>,
+    comp: Texture<'a>,
+    current: Option<Texture<'a>>,
 }
 
-// requires lifetime specifier because textures aren't allowed to outlive texture_creators
 impl<'a> Split<'a> {
     pub fn new(
-        pb_time: u128,
-        gold_time: u128,
-        diff: i128,
-        diff_texture: Option<Texture<'a>>,
-        name_texture: Texture<'a>,
-        comp_texture: Texture<'a>,
-        current_texture: Option<Texture<'a>>,
+        name: Texture<'a>,
+        comp: Texture<'a>,
+        diff: Option<Texture<'a>>,
+        current: Option<Texture<'a>>,
     ) -> Self {
         Self {
-            pb_time,
-            gold_time,
             diff,
-            diff_texture,
-            name_texture,
-            comp_texture,
-            current_texture,
+            name,
+            comp,
+            current,
         }
     }
-    pub fn time(&self) -> u128 {
-        self.pb_time
-    }
-    pub fn set_time(&mut self, time: u128) {
-        self.pb_time = time;
-    }
     pub fn name(&self) -> &Texture {
-        &self.name_texture
+        &self.name
     }
-    pub fn comp_texture(&self) -> &Texture {
-        &self.comp_texture
+    pub fn comp(&self) -> &Texture {
+        &self.comp
     }
-    pub fn set_comp_tex(&mut self, tex: Texture<'a>) {
-        self.comp_texture = tex;
+    pub fn set_comp(&mut self, tex: Texture<'a>) {
+        self.comp = tex;
     }
     pub fn cur(&self) -> &Option<Texture> {
-        &self.current_texture
+        &self.current
     }
     pub fn set_cur(&mut self, cur: Option<Texture<'a>>) {
-        self.current_texture = cur;
+        self.current = cur;
     }
-    pub fn set_diff(&mut self, diff: i128, texture: Option<Texture<'a>>) {
-        self.diff = diff;
-        self.diff_texture = texture;
+    pub fn set_diff(&mut self, texture: Option<Texture<'a>>) {
+        self.diff = texture;
     }
-    pub fn diff(&self) -> i128 {
-        self.diff
-    }
-    pub fn diff_texture(&self) -> &Option<Texture<'a>> {
-        &self.diff_texture
-    }
-    pub fn gold(&self) -> u128 {
-        self.gold_time
-    }
-    pub fn set_gold(&mut self, gold: u128) {
-        self.gold_time = gold;
+    pub fn diff(&self) -> &Option<Texture<'a>> {
+        &self.diff
     }
 }
