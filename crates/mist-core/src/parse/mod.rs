93d2dc7636c6cdef56e7e06bc1b13ad8bd9afaaf
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2022-06-06 09:58:47 -0700

    [core] run sanitizer resize if too many splits also
    previously it would only resize the vecs if there were too few members,
    now remove excess ones as well

--- crates/mist-core/src/parse/mod.rs
+++ crates/mist-core/src/parse/mod.rs
@@ -15,15 +15,15 @@ pub(crate) fn sanify_run(run: &Run) -> Run {
     let mut golds = run.gold_times().to_owned();
     let mut times = run.pb_times().to_owned();
     let mut sums = run.sum_times().to_owned();
-    if golds.len() < len {
+    if golds.len() != len {
         golds.resize_with(len, Default::default);
         run.set_gold_times(&golds);
     }
-    if times.len() < len {
+    if times.len() != len {
         times.resize_with(len, Default::default);
         run.set_pb_times(&times);
     }
-    if sums.len() < len {
+    if sums.len() != len {
         sums.resize_with(len, Default::default);
         run.set_sum_times(&sums);
     }
