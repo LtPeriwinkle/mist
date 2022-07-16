bd3196779aa3faa4de01318f59e7860624f45ef8
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2022-07-15 21:56:06 -0700

    [core] add state feature; remove timing feature

--- crates/mist-core/src/timer/mod.rs
+++ crates/mist-core/src/timer/mod.rs
@@ -1,9 +1,13 @@
 //! Everything related to timing in mist
+#[cfg(feature = "state")]
 mod comparison;
 pub mod format;
 mod instant;
 mod run;
+#[cfg(feature = "state")]
 pub mod state;
 mod time_type;
+#[cfg(feature = "state")]
+pub use comparison::Comparison;
 pub use time_type::{DiffType, TimeType};
-pub use {comparison::Comparison, instant::MistInstant, run::Run};
+pub use {instant::MistInstant, run::Run};
